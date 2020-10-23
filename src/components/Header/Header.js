bed60c55ef3c4da118d0e62406e726733fb93c3d
Koffi Nyonouglo <nyonouglokoffi8@gmail.com>
2020-10-22 20:26:28 -0700

    Simpe E-Commerce App

---
+++ src/components/Header/Header.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {NavLink} from 'react-router-dom';
+import './header.css';
+
+const Header = ({cartLength}) => {
+
+
+    const openMenu = () =>{
+        document.querySelector(".sidebar").classList.add("open");
+      }
+      const closeMenu = () => {
+        document.querySelector(".sidebar").classList.remove("open");
+      }
+  
+
+    return (
+
+         <div className="grid-container">
+
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+            <div className="container">
+
+            <div className="brand">
+            <button onClick={openMenu}>&#9776;Products</button>
+           
+              </div>
+
+
+                <div>
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to={"/cart"}><i className="fa fa-shopping-cart mr-2"
+                            aria-hidden="true" />Cart {cartLength ? `(${cartLength})`: ''}</NavLink>
+
+                           
+                      
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+
+        <aside className="sidebar">
+
+
+
+        <div className="card mb-3">
+                <div className="card-header">
+                 <br/><br/>
+                </div>
+                <ul className="list-group flex-row flex-wrap">
+                <button className="sidebar-close-button" onClick={closeMenu}>
+            x
+          </button>
+          <ul className="categories">
+          <NavLink className="navbar-brand" to="/">Products</NavLink>
+
+          </ul>
+
+                </ul>
+            </div>
+
+
+
+
+        </aside>
+
+
+
+        </div>
+    );
+};
+
+
+const mapStateToProps = (state) => {
+  return {
+      cartLength: state.shop.cart.length
+  }
+};
+
+export default connect(mapStateToProps, null)(Header);
+
+
+
+                        // <li className="nav-item active">
+                        //     <a className="nav-link" href="#">Home
+                        //         <span className="sr-only">(current)</span>
+                        //     </a>
+                        // </li>
+                        // <li className="nav-item">
+                        //     <a className="nav-link" href="#">About</a>
+                        // </li>
+                        // <li className="nav-item">
+                        //     <a className="nav-link" href="#">Services</a>
+                        // </li>
+                        // <li className="nav-item">
+                        //     <a className="nav-link" href="#">Contact</a>
+                        // </li>
